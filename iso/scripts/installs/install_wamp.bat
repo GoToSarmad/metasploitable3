70524474f9e74aea601160285abcb408cb682fb7
Matt Robinson <brimstone@the.narro.ws>
2018-02-18 13:55:38 -0500

    Fix wamp url

--- iso/scripts/installs/install_wamp.bat
+++ iso/scripts/installs/install_wamp.bat
@@ -1,4 +1,4 @@
-powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://sourceforge.net/projects/wampserver/files/WampServer 2/WampServer 2.2/wampserver2.2d-x64.exe', 'C:\Windows\Temp\wampserver2.2.d-x64.exe')" <NUL
+powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://svwh.dl.sourceforge.net/project/wampserver/WampServer 2/WampServer 2.2/wampserver2.2d-x64.exe', 'C:\Windows\Temp\wampserver2.2.d-x64.exe')" <NUL
 C:\Windows\Temp\wampserver2.2.d-x64.exe /verysilent
 copy /Y "D:\resources\wamp\httpd.conf" "C:\wamp\bin\apache\Apache2.2.21\conf\httpd.conf"
 copy /Y "D:\resources\wamp\phpmyadmin.conf" "C:\wamp\alias\phpmyadmin.conf"
